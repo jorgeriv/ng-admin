328079afbffd990c05ae9256064ba89b6b98cb13
Jorge Rivera <jorgeriv@gmail.com>
2015-02-10 01:04:54 -0600

    add timetable route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var customer = require('./customer');
 var place = require('./place');
+var timetable = require('./timetable');
+
 module.exports = function(app){
 
   // Customer
@@ -17,5 +19,11 @@ module.exports = function(app){
   app.post('/place', place.add);
   app.put('/place/:id', place.update);
   app.delete('/place/:id', place.delete);
-  
+
+  // Place
+  app.get('/timetable', timetable.getAll);
+  app.get('/timetable/:id', timetable.get);
+  app.post('/timetable', timetable.add);
+  app.put('/timetable/:id', timetable.update);
+  app.delete('/timetable/:id', timetable.delete);
 };
